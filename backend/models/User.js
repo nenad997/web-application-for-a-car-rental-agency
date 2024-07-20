b7a02de73d78c40dcf8836cc5d49d86e840bdd69
Nenad <nenad.matijevic97@hotmail.com>
2024-07-20 11:04:04 +0200

    add User model

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    idCardNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
