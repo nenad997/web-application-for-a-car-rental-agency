89fdd65f74a9e81c34fd49b96e1d3ac06c0e6e15
Nenad <nenad.matijevic97@hotmail.com>
2024-07-21 13:05:44 +0200

    add getCaById controller

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -33,15 +33,15 @@ exports.addNewCar = async (req, res, next) => {
     price,
   } = req.body;
 
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.json({
-        message: "Validation failed",
-        data: errors.array(),
-        status: 403,
-      });
-    }
+  if (!errors.isEmpty()) {
+    return res.json({
+      message: "Validation failed",
+      data: errors.array(),
+      status: 403,
+    });
+  }
 
   const newCar = new Car({
     vehicleModel,
@@ -53,13 +53,13 @@ exports.addNewCar = async (req, res, next) => {
     price,
   });
 
-    const newCarResult = await newCar.save();
+  const newCarResult = await newCar.save();
 
-    if (!newCarResult) {
-      const error = new Error("Failed to add new car");
-      error.status = 400;
-      throw error;
-    }
+  if (!newCarResult) {
+    const error = new Error("Failed to add new car");
+    error.status = 400;
+    throw error;
+  }
 
   res.json({
     message: "Added new car successfully",
@@ -68,12 +68,22 @@ exports.addNewCar = async (req, res, next) => {
   });
 };
 
-// {
-//   "vehicleMake": "Renault",
-//   "vehicleModel": "Clio 1.2",
-//   "registrationNumber": "ŠI - 22 - MM",
-//   "imageUrl": "https://images.unsplash.com/photo-1666335009164-2597314da8e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "moreInfo": "Nice Car. Test",
-//   "fuel": "Diesel",
-//   "price": 25
-// }
+exports.getCarById = async (req, res, next) => {
+  try {
+    const { carId } = req.params;
+
+    const fetchedCar = await Car.findById(carId);
+
+    if (!fetchedCar) {
+      const error = new Error("Could not fetch a car");
+      error.status = 404;
+    }
+
+    res.status(200).json({
+      message: "Success",
+      car: fetchedCar,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
