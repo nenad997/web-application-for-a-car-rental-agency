0f2b7a94056a48a197fd0d7785651c966198b45a
Nenad <nenad.matijevic97@hotmail.com>
2024-11-22 11:27:41 +0100

    Add folder structure for storing uploaded images
    - Created 'uploads/images' directory for organizing uploaded files.
    - Updated server logic to save images in the new folder structure.

--- backend/app.mjs
+++ backend/app.mjs
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static("./uploads"));
+app.use("/uploads", express.static("./uploads/images"));
 
 app.use("/api", authRoutes);
 app.use("/api", feedRoutes);
