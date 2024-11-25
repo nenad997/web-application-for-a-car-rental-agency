ae7a8bc255038890c266cd0cce52310df8939e80
Nenad <nenad.matijevic97@hotmail.com>
2024-11-25 20:56:47 +0100

    Add Toastify feedback for car deletion errors
    - Integrated React Toastify to notify users of errors during car deletion.
    - Enhanced error handling logic to display meaningful feedback.
    - Improved user experience by providing immediate visual cues for failed operations.

--- frontend/src/components/forms/CarForm.jsx
+++ frontend/src/components/forms/CarForm.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import {
   Form,
   useActionData,
@@ -10,18 +10,19 @@ import {
 } from "react-router-dom";
 import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
+import { ToastContainer } from "react-toastify";
 
 import classes from "./CarForm.module.css";
 import Input, { SelectInput } from "../ui/Input";
 import ImagePickerInput from "../ui/ImagePicker";
 import { getAuthToken } from "../../util/authorization";
 import { filterError } from "../../util/error-filter";
+import { generateToast } from "../../util/toastify";
 
 const fuelsString =
   "None, G-Drive Diesel, G-Drive 100, OPTI Diesel, OPTI Benzin 95, OPTI Auto Gas, Euro Diesel, Euro Premium BMB95, Metan CNG, Electrical Charger";
 
 const CarForm = memo(({ method, car }) => {
-  const [error, setError] = useState();
   const actionData = useActionData();
   const navigation = useNavigation();
   const { carId } = useParams();
@@ -49,7 +50,9 @@ const CarForm = memo(({ method, car }) => {
       navigate("/");
       revalidator.revalidate();
     } catch (err) {
-      setError(err.message);
+      generateToast(err.message, {
+        color: "red",
+      });
     }
   };
 
@@ -85,143 +88,145 @@ const CarForm = memo(({ method, car }) => {
   };
 
   return (
-    <Form
-      className={classes.form}
-      method={method}
-      encType="multipart/form-data"
-    >
-      {error && <p className={classes.error}>{error}</p>}
-      <div className={classes.control}>
-        <Input
-          label="Vehicle Make *"
-          config={{
-            type: "text",
-            id: "make",
-            name: "vehicleMake",
-            placeholder: "Enter vehicle make (Renault)",
-            defaultValue: car?.vehicleMake ?? "",
-          }}
-          hasError={errors.makeErr}
-          errorText={errors.makeErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <Input
-          label="Vehicle Model *"
-          config={{
-            type: "text",
-            id: "model",
-            name: "vehicleModel",
-            placeholder: "Enter vehicle model (Clio 1.2)",
-            defaultValue: car?.vehicleModel ?? "",
-          }}
-          hasError={errors.modelErr}
-          errorText={errors.modelErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <ImagePickerInput
-          currentImage={car?.image ?? null}
-          hasError={errors.imageErr}
-          errorText={errors.imageErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <Input
-          label="Price per day *"
-          config={{
-            type: "text",
-            id: "price",
-            name: "price",
-            placeholder: "Enter a vehicle price (per day)",
-            defaultValue: car?.price ?? "",
-          }}
-          hasError={errors.priceErr}
-          errorText={errors.priceErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <Input
-          label="Registration number *"
-          config={{
-            type: "text",
-            id: "registration-number",
-            name: "registrationNumber",
-            placeholder: "Format (CC - 123 - AA)",
-            defaultValue: car?.registrationNumber ?? "",
-          }}
-          hasError={errors.regErr}
-          errorText={errors.regErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <Input
-          label="Registration expiration date *"
-          config={{
-            type: "date",
-            id: "reg-exp",
-            name: "regExpiration",
-            defaultValue: car?.regExpiration.split("T")[0] ?? "",
-          }}
-          hasError={errors.dateErr}
-          errorText={errors.dateErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <label className="text-label" htmlFor="more-info">
-          More Info {"(optional)"}
-        </label>
-        <textarea
-          name="moreInfo"
-          id="more-info"
-          placeholder="Enter more information about vehicle"
-          defaultValue={car?.moreInfo ?? ""}
-        />
-      </div>
-      <div className={classes.control}>
-        <SelectInput
-          label="Type of fuel *"
-          config={{
-            type: "text",
-            id: "fuel",
-            name: "fuel",
-            defaultValue: car?.fuel ?? "",
-          }}
-          options={fuelsString
-            .split(",")
-            .map((item) => item.trim().toUpperCase())}
-          hasError={errors.fuelErr}
-          errorText={errors.fuelErr?.message}
-        />
-      </div>
-      <div className={classes.control}>
-        <button
-          className={`${classes.button} ${classes["add-button"]}`}
-          type="submit"
-          title="Confirm"
-        >
-          {isSubmitting ? "Submitting" : !car ? "Add" : "Edit"}
-        </button>
-        {car && (
+    <>
+      <ToastContainer />
+      <Form
+        className={classes.form}
+        method={method}
+        encType="multipart/form-data"
+      >
+        <div className={classes.control}>
+          <Input
+            label="Vehicle Make *"
+            config={{
+              type: "text",
+              id: "make",
+              name: "vehicleMake",
+              placeholder: "Enter vehicle make (Renault)",
+              defaultValue: car?.vehicleMake ?? "",
+            }}
+            hasError={errors.makeErr}
+            errorText={errors.makeErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <Input
+            label="Vehicle Model *"
+            config={{
+              type: "text",
+              id: "model",
+              name: "vehicleModel",
+              placeholder: "Enter vehicle model (Clio 1.2)",
+              defaultValue: car?.vehicleModel ?? "",
+            }}
+            hasError={errors.modelErr}
+            errorText={errors.modelErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <ImagePickerInput
+            currentImage={car?.image ?? null}
+            hasError={errors.imageErr}
+            errorText={errors.imageErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <Input
+            label="Price per day *"
+            config={{
+              type: "text",
+              id: "price",
+              name: "price",
+              placeholder: "Enter a vehicle price (per day)",
+              defaultValue: car?.price ?? "",
+            }}
+            hasError={errors.priceErr}
+            errorText={errors.priceErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <Input
+            label="Registration number *"
+            config={{
+              type: "text",
+              id: "registration-number",
+              name: "registrationNumber",
+              placeholder: "Format (CC - 123 - AA)",
+              defaultValue: car?.registrationNumber ?? "",
+            }}
+            hasError={errors.regErr}
+            errorText={errors.regErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <Input
+            label="Registration expiration date *"
+            config={{
+              type: "date",
+              id: "reg-exp",
+              name: "regExpiration",
+              defaultValue: car?.regExpiration.split("T")[0] ?? "",
+            }}
+            hasError={errors.dateErr}
+            errorText={errors.dateErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
+          <label className="text-label" htmlFor="more-info">
+            More Info {"(optional)"}
+          </label>
+          <textarea
+            name="moreInfo"
+            id="more-info"
+            placeholder="Enter more information about vehicle"
+            defaultValue={car?.moreInfo ?? ""}
+          />
+        </div>
+        <div className={classes.control}>
+          <SelectInput
+            label="Type of fuel *"
+            config={{
+              type: "text",
+              id: "fuel",
+              name: "fuel",
+              defaultValue: car?.fuel ?? "",
+            }}
+            options={fuelsString
+              .split(",")
+              .map((item) => item.trim().toUpperCase())}
+            hasError={errors.fuelErr}
+            errorText={errors.fuelErr?.message}
+          />
+        </div>
+        <div className={classes.control}>
           <button
-            className={`${classes.button} ${classes["delete-button"]}`}
-            type="button"
-            title="Delete"
-            onClick={confirmDeletion}
+            className={`${classes.button} ${classes["add-button"]}`}
+            type="submit"
+            title="Confirm"
           >
-            Delete
+            {isSubmitting ? "Submitting" : !car ? "Add" : "Edit"}
           </button>
-        )}
-        <Link
-          to=".."
-          className={`${classes.button} ${classes["cancel-button"]}`}
-          title="Cancel"
-        >
-          Cancel
-        </Link>
-      </div>
-      <p className="text">* REQUIRED FIELDS</p>
-    </Form>
+          {car && (
+            <button
+              className={`${classes.button} ${classes["delete-button"]}`}
+              type="button"
+              title="Delete"
+              onClick={confirmDeletion}
+            >
+              Delete
+            </button>
+          )}
+          <Link
+            to=".."
+            className={`${classes.button} ${classes["cancel-button"]}`}
+            title="Cancel"
+          >
+            Cancel
+          </Link>
+        </div>
+        <p className="text">* REQUIRED FIELDS</p>
+      </Form>
+    </>
   );
 });
 
